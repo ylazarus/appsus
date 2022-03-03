cb5b5bd9e1a1c741af3dff8d2847d37f01bc53d1
Yoni Lazarus <ylazarus@gmail.com>
2022-03-03 20:00:55 +0200

    thursday submit

--- js/apps/mail/pages/displayed-mail.cmp.js
+++ js/apps/mail/pages/displayed-mail.cmp.js
@@ -1,22 +1,27 @@
 import { mailService } from "../services/mail.service.js"
 import { eventBus } from "../../../services/eventBus-service.js"
 
-
 export default {
   template: `
         <section class="displayed-email-page main-layout">
-          <h1>Email</h1>
-          <div>
+          <h1 class="mail-title">Email</h1>
+          <div class="mail-display-card flex flex-column">
 
-            <h1>{{mail.subject}}</h1>
+            <h1>Subject: {{mail.subject}}</h1>
             <h3>From: {{mail.from}}</h3>
-            <p>{{mail.txt}}</p>
-            <button class="btn" @click="markUnread">Mark as Unread</button>
-            <button class="btn" @click="deleteMessage">Delete Message</button>
-            <router-link class="btn" :to="'/compose/'+mail.id">Reply</router-link>
-            <router-link class="btn" :to="'/mail/'+mail.prevMailId">Prev Email</router-link> | 
-            <router-link class="btn" :to="'/mail/'+mail.nextMailId">Next Email</router-link> | 
-            <router-link class="btn" to="/mail">Back</router-link>
+            <p class="mail-txt">{{mail.txt}}</p>
+              
+            <div class="first-buttons-row flex space-between">
+                <button class="btn" @click="markUnread">Mark as Unread</button>
+                <button class="btn" @click="deleteMessage">Delete Message</button>
+                <router-link class="btn" :to="'/compose/'+mail.id">Reply</router-link>
+            </div>
+              
+            <div class="second-buttons-row flex space-between">
+                <router-link class="btn" :to="'/mail/'+mail.prevMailId">Prev Email</router-link>
+                <router-link class="btn" :to="'/mail/'+mail.nextMailId">Next Email</router-link>
+                <router-link class="btn" to="/mail">Back To Inbox</router-link>
+            </div>
           </div>
         </section>
             
@@ -50,19 +55,14 @@ export default {
     },
     markUnread() {
       this.mail.isRead = false
-      mailService.save(this.mail)
-      .then(mail => this.mail=mail)
+      mailService.save(this.mail).then((mail) => (this.mail = mail))
     },
-    deleteMessage(){
-      mailService.deleteMail(this.mail.id)
-      .then(mail => {
+    deleteMessage() {
+      mailService.deleteMail(this.mail.id).then((mail) => {
         eventBus.emit("show-msg", { txt: "Your Message Has Been Deleted" })
         this.$router.push("/mail")
       })
     },
-    reply(){
-
-    }
-
+    reply() {},
   },
 }
