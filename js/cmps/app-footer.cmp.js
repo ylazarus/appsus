3528ec4fe9d9c9935d0c11cac7d46d4fd16d7b5f
Yoni Lazarus <ylazarus@gmail.com>
2022-03-04 00:34:34 +0200

    finally centered the damned links in the header

--- js/cmps/app-footer.cmp.js
+++ js/cmps/app-footer.cmp.js
@@ -2,7 +2,7 @@ export default{
     template: `
         <footer class="footer-container flex">
             <!-- <nav class="app-header main-layout flex space-between"> -->
-                <router-link class="footer-link clean-link" to="/about">&copy coffeerights Appsus 2022</router-link>
+                <p class="footer-link clean-link" to="/about">&copy coffeerights Appsus 2022</p>
                 
             <!-- </nav> -->
         </footer>
--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -3,11 +3,11 @@ export default{
         <header class="header-container">
             <nav class="app-header main-layout flex space-between">
                 <router-link class="appsus-link clean-link" to="/">Appsus</router-link>
-                <ul class="header-links flex clean-list">
-                    <li><router-link class="keepsus-link clean-link" to="/mail">Mailsus</router-link></li>
-                    <li><router-link class="keepsus-link clean-link" to="/keep">Keepsus</router-link></li>
-                    <li><router-link class="about-link clean-link" to="/about">About</router-link></li>
-                </ul>
+                <div class="header-links flex clean-list">
+                    <router-link class="keepsus-link clean-link" to="/mail">Mailsus</router-link>
+                    <router-link class="keepsus-link clean-link" to="/keep">Keepsus</router-link>
+                    <router-link class="about-link clean-link" to="/about">About</router-link>
+                </div>
             </nav>
         </header>
     `,
