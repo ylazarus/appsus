8e7b0191e05130b1ab18a73014a1e8b4f8a3ad60
Yoni Lazarus <ylazarus@gmail.com>
2022-03-04 00:13:25 +0200

    added user message cmp

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 import { router } from "./router.js";
 import appHeader from "./cmps/app-header.cmp.js";
 import appFooter from "./cmps/app-footer.cmp.js";
+import userMsg from "./cmps/user-msg.cmp.js";
 
 
 const options = {
     template: `
         <section>
             <app-header />
-            <!-- <user-msg /> -->
+            <user-msg />
             <router-view />
             <app-footer />
         </section>
     `,
     components:{
        appHeader,
-       appFooter
-    //    userMsg 
+       appFooter,
+       userMsg
     }
 };
 
